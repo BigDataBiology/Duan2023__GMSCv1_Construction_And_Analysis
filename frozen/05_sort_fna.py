8043ee8f8d6d63735cffd3fe6e416541a3ea2998
cocodyq <55238881+cocodyq@users.noreply.github.com>
2021-09-13 16:24:10 +0800

    Add codes for generating frozen files

---
+++ frozen/05_sort_fna.py
@@ -0,0 +1,63 @@
+'''
+Concept:
+Sort 100AA fna.
+'''
+
+import hashlib 
+import lzma
+from fasta import fasta_iter
+import heapq
+from glob import glob
+
+'''
+Split 100 fna file into 256 subfiles.
+'''
+def splitseq(infile):
+    print("start splitseq")
+    outputlist = [
+        f'/data/frozen/split/sub_{ix:03}.fna.xz'
+        for ix in range(256)]
+    outputfiles = [
+        lzma.open(f'/data/frozen/split/sub_{ix:03}.fna.xz',"wt")
+        for ix in range(256)]
+    for ID,seq in fasta_iter(infile):
+        h = hashlib.sha256()
+        h.update(seq.encode('ascii'))
+        ix = int(h.hexdigest()[:2], 16)
+        outputfiles[ix].write(f'>{ID}\n{seq}\n')
+    for ot in outputfiles:
+        ot.close()
+    print("finish splitseq")
+    return (outputlist)
+    
+'''
+De duplicate and sort each subfiles.
+'''
+def dedup_fasta(infile):
+    print("start dedup")
+    fasta = {}
+    for ID,seq in fasta_iter(infile):
+        fasta[ID] = seq
+    outfile = infile.replace('.fna.xz', '.sort.fna.xz')
+    out = lzma.open(outfile, "wt")
+    print("start sort")
+    for ID,seq in sorted(fasta.items()):
+        out.write(f">{ID}\n{seq}\n")
+    out.close()
+
+def merge(outfile):
+    with lzma.open(outfile, "wt") as output:
+        inputs = [fasta_iter(f) for f in glob(f'/data/frozen/split/*.sort.fna.xz')]
+        merged = heapq.merge(*inputs, key=lambda h_seq: (h_seq[0], h_seq[1]))
+        for h,seq in merged:
+            output.write(f'>{h}\n{seq}\n')
+
+INPUT_FILE = "/data/frozen/100AA_GMSC.fna.xz"
+OUTPUT_FILE="/data/frozen/sort/100AA_GMSC_sort.fna.xz"
+
+splits = splitseq(INPUT_FILE)
+
+for sp in splits:
+    dedup_fasta(sp)
+
+merge(OUTPUT_FILE)
---
+++ frozen/10_all_taxonomy.py
@@ -0,0 +1,79 @@
+'''
+Generate all level taxonomy mapping file.
+Columns:
+100AA smORF accession
+90AA smORF accession
+50AA smORF accession
+Taxonomy at 100AA
+Taxonomy at 90AA
+Taxonomy at 50AA
+'''
+
+def map_taxonomy(infile1,infile2,infile3,infile4,outfile):
+    import lzma
+    habitat100 = {}
+    habitat90 = {}
+    habitat50 = {}
+    out = lzma.open(outfile, "wt")
+    with lzma.open(infile1,"rt") as f1:
+        for line in f1:
+            line = line.strip()
+            linelist = line.split("\t",1)
+            if len(linelist) == 2:
+                habitat100[linelist[0]] = linelist[1].replace("\t",";")
+            else:
+                continue
+    with lzma.open(infile2,"rt") as f2:
+        for line in f2:
+            line = line.strip()
+            linelist = line.split("\t",1)
+            if len(linelist) == 2:
+                habitat90[linelist[0]] = linelist[1].replace("\t",";")
+            else:
+                continue
+    with lzma.open(infile3,"rt") as f3:
+        for line in f3:
+            line = line.strip()
+            linelist = line.split("\t",1)
+            if len(linelist) == 2:
+                habitat50[linelist[0]] = linelist[1].replace("\t",";")
+            else:
+                continue
+    with lzma.open(infile4,"rt") as f4:
+        for line in f4:
+            line = line.strip()
+            linelist = line.split("\t")             
+            out.write(line+"\t")
+            if len(linelist) == 3:
+                if linelist[0] in habitat100.keys():
+                    out.write(habitat100[linelist[0]]+"\t")
+                else:
+                    out.write(""+"\t")
+                if linelist[1] in habitat90.keys():
+                    out.write(habitat90[linelist[1]]+"\t")
+                else:
+                    out.write(""+"\t")            
+                if linelist[2] in habitat50.keys():
+                    out.write(habitat50[linelist[2]]+"\n")
+                else:
+                    out.write(""+"\n")       
+            else:
+                out.write(""+"\t")
+                if linelist[0] in habitat100.keys():
+                    out.write(habitat100[linelist[0]]+"\t")
+                else:
+                    out.write(""+"\t")
+                if linelist[1] in habitat90.keys():
+                    out.write(habitat90[linelist[1]]+"\t")
+                else:
+                    out.write(""+"\t")            
+                out.write(""+"\n")                  
+    out.close()
+       
+INPUT_FILE_1 = "/data/frozen/100AA_taxonomy.tsv.xz"   
+INPUT_FILE_2 = "/data/frozen/90AA_ref_taxonomy.tsv.xz"
+INPUT_FILE_3 = "/data/frozen/50AA_ref_taxonomy.tsv.xz"
+INPUT_FILE_4 = "/data/frozen/all_0.9_0.5_family_sort.tsv.xz"
+OUTPUT_FILE_1 = "/data/frozen/all_taxonomy.tsv.xz" 
+
+map_taxonomy(INPUT_FILE_1,INPUT_FILE_2,INPUT_FILE_3,INPUT_FILE_4,OUTPUT_FILE_1)
