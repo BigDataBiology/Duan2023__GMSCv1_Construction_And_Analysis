53d2fd840d943a9ee56720aca49c16820caf309b
cocodyq <1057696203@qq.com>
2024-05-16 15:41:54 +0800

    ENH update 06_Compare_with_other_datasets

--- General_Scripts/06_Compare_with_other_datasets/02_filter_sp_dedup.py
+++ General_Scripts/06_Compare_with_other_datasets/02_filter_sp_dedup.py
@@ -11,11 +11,7 @@ def filter_dedup(infile,outfile):
                     out.write(f'>{header}\n{seq}\n')
                     seqset.add(seq)
 
-infile1 = '~/refseq/arc/refseq_arc.faa.gz'
-outfile1 = '~/refseq/arc/refseq_sp_arc_dedup.faa'
-
-infile2 = '~/refseq/bac/refseq_bac.faa.gz'
-outfile2 = '~/refseq/bac/refseq_sp_bac_dedup.faa'
+infile1 = 'database.faa'
+outfile1 = 'database_dedup.faa'
 
 filter_dedup(infile1,outfile1)
-filter_dedup(infile2,outfile2)
